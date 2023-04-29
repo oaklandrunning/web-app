01c92d40281e0a27ee5262b230f79dce16673347
oaklandrunning <noreply@gmail.com>
2023-04-29 08:40:55 -0700

    updates from Linux

---
+++ index.js
@@ -0,0 +1,71 @@
+let notes = [
+    {
+        id: new Date(), 
+        title: 'Sample Notessdfdf', 
+        body: 'This is a description for our notes',
+        bgColor: 'pink'
+    }
+]
+
+
+const createElement = (tag, classes =[]) => {
+    const element = document.createElement(tag);
+    classes.forEach(cl => {
+        element.classList.add(cl);
+    })
+    return element;
+
+}
+
+const createNoteView = (note) => {
+    const noteDiv = createElement('div', ['note']);
+    noteDiv.id = note.id;
+    const textDiv = createElement('div', ['text']);
+    textDiv.style.background = note.bgColor;
+    const titleP = createElement('b', ['title']);
+    titleP.innerHTML = note.title;
+    const bodyP = createElement('p', ['body']);
+    bodyP.innerHTML = note.body;
+    const editButton = createElement('button', ['edit']);
+    editButton.innerHTML = 'Edit Note';
+    const deleteButton = createElement('button', ['delete']);
+    deleteButton.innerHTML = 'Delete Note';
+
+    textDiv.append(titleP)
+    textDiv.append(bodyP)
+    noteDiv.append(textDiv)
+    noteDiv.append(editButton)
+    noteDiv.append(deleteButton)
+
+    deleteButton.onclick = () => deleteNote(noteDiv);
+    return noteDiv;
+}
+
+const saveNote = () => {
+    const titleInput = document.querySelector('input#title');
+    const bodyInput = document.querySelector('input#body');
+    const bgColorInput = document.querySelector('select');
+    const id = new Date().getTime();
+    const note = {
+        id, title: titleInput.value, body: bodyInput.value, bgColor: bgColorInput.value
+    }
+    const noteDiv = createNoteView(note);
+    notesDiv.prepend(noteDiv);
+    titleInput.value = '';
+    bodyInput.value = '';
+    bgColorInput.value = 'Select Color';
+}
+
+const deleteNote =(noteDiv) => {
+    noteDiv.remove();
+    notes = notes.filter(note => note.id != noteDiv.id);
+}
+
+document.querySelector('button.add').onclick = () => saveNote();
+
+const notesDiv = document.querySelector('.notesDiv');
+
+notes.forEach(note => {
+    const noteDiv = createNoteView(note);
+    notesDiv.append(noteDiv);
+})
